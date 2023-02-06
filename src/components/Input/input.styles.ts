d72e7f1bea8d4b4dfaa5eb5f057155885e247acf
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-06 19:55:23 -0300

    feat: add Input styles

---
+++ src/components/Input/input.styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const InputStyled = styled.input `
+    border: none;
+    border-radius: 1rem;
+    background-color: #242424;
+    height: 40px;
+    padding: 0.5rem;
+    color: white;
+    position: absolute;
+    
+    ::placeholder {
+        color:#b6b6b6;
+    }
+`
