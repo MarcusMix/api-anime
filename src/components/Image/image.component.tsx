16ed0b2a4240b7252a43bd9afb53b8b92ee65708
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-06 19:55:33 -0300

    feat: Image component

---
+++ src/components/Image/image.component.tsx
@@ -0,0 +1,13 @@
+//styles
+import { ImageStyled } from "./image.styles"
+
+//interface
+import ImageProps from "./image.types"
+
+const Image = ({ src, alt}: ImageProps) => {
+    return (
+        <ImageStyled src={src} alt={alt} />
+    )
+}
+
+export default Image
