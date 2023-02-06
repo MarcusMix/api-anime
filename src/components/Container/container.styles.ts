86229d1c4cce1f63f105f235b9588d72ab6fb436
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-06 19:54:54 -0300

    feat: add Container styles

---
+++ src/components/Container/container.styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ContainerStyled = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    padding: 1rem;
+    /* margin: 1rem; */
+    margin: 0 auto;
+    background-color: #eceee9;
+    max-width: 1280px;
+`
