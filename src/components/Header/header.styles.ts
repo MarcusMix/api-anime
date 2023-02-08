4700aea304aeb17495fd2997c0dde98c3e518947
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-08 10:46:13 -0300

    feat: add header styles

---
+++ src/components/Header/header.styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const HeaderStyled = styled.header `
+    display: flex;
+    background-color: #eceee9;
+    height: 80px;
+    margin: 0;
+    padding: 0;
+`
