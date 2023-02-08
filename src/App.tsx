80b5ee84f0c7735dc705d2a5a79ce74ae096d7d2
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-08 10:47:37 -0300

    feat: add icons and new info about animes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,13 @@ import Input from './components/Input/input.component'
 import Card from './components/Card/card.component'
 import Container from './components/Container/container.component'
 import Image from './components/Image/image.component'
+import Header from './components/Header/header.component'
+
+//icons
+import { BiCameraMovie } from 'react-icons/bi'
+import { MdFormatListNumbered, MdOutlineGrade } from 'react-icons/md'
+import { VscDebugStart } from 'react-icons/vsc'
+import { BsFillCalendarCheckFill } from 'react-icons/bs'
 
 //styles
 import './App.css'
@@ -22,29 +29,32 @@ const api = "https://kitsu.io/api/edge/"
 
 function App() {
 
-
-  const [text, setText] = useState<string>('')
+  const [text, setText] = useState<string>('naruto')
 
   const [info, setInfo] = useState<any>({})
+
+  const [isOpen, setIsOpen] = useState<boolean>(false)
 
   useEffect(() => {
     setInfo({})
     fetchAnimes()   
   }, [text])
+  
 
   const fetchAnimes = async ()  => {
     const response = await fetch(`${api}anime?filter[text]=${text}&page[limit]=16`)
     const dataAnimes = await response.json()
     setInfo(dataAnimes)
+    console.table(info.data)
   }
 
 
   return (
     <div className="App">
-      <Container>
-      <img className='back' src={background} alt="" />
-        <h1>Animes</h1>
-      </Container>
+      <Header>
+      <img className='back' src={background} alt="background" />
+      <h1>Animes</h1>
+      </Header>
 
       <Container>
       <Input 
@@ -58,6 +68,7 @@ function App() {
           {info.data.map((anime: any) => {
             return (  
             <Card 
+              onClick={() => setIsOpen(!isOpen)} 
               key={anime.id}
               layout
               animate={{opacity: 1}} 
@@ -68,7 +79,16 @@ function App() {
                 src={anime.attributes.posterImage.small} 
                 alt={anime.attributes.canonicalTitle} 
               />
-              <p>{anime.attributes.canonicalTitle}</p>
+              <p><strong>{anime.attributes.canonicalTitle}</strong></p>
+              <p> <BiCameraMovie/> Episódios: {anime.attributes.episodeCount}</p>
+              <p> <MdFormatListNumbered/> Tamanho: {anime.attributes.episodeLength} min.</p>
+              <p> <MdOutlineGrade/> Nota: {anime.attributes.averageRating}</p>
+              <p> <VscDebugStart/> Início: {anime.attributes.startDate}</p>
+              <p> <BsFillCalendarCheckFill/> Fim: {anime.attributes.endDate}</p>
+
+              {isOpen && (
+                <p>{anime.attributes.synopsis}</p>
+              )}
             </Card>
             )
           })}
