c066e9085a04d0e063d0e6aa0c9df36ab2b6058c
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-08 10:47:58 -0300

    style: centralize icons

--- src/components/Card/card.styles.ts
+++ src/components/Card/card.styles.ts
@@ -5,6 +5,8 @@ export const CardContainer = styled(motion.li) `
     display: flex;
     justify-content: center;
     flex-direction: column;
+    text-align: center;
+    align-self: center;
     margin: 1rem;
     flex-wrap: wrap;
     max-width: 250px;
@@ -13,11 +15,18 @@ export const CardContainer = styled(motion.li) `
     box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
     font-family: 'Fira Sans';
     background-color: white;
+   
     
-    & p {
+    & p { 
         padding: 0.5rem;
         text-align: start;
         margin-top: 0.3rem;
         background-color: white;
     }
+    
+    & svg {
+        vertical-align: -10%;
+    }
+
+    
 `
