6c36ed5a77b0b96a5a25e32934b2b96de4a2e13c
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-19 17:02:15 -0300

    feat: add anime details route and home

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+//pages
+import AnimeDetail from './pages/AnimeDetails.page'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path={'/'} element={<App />}/>
+      <Route path={'/anime/:id'} element={<AnimeDetail/>}/>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
