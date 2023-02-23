ee29fc8bfbe47206db5d276616233852e3d1a7ef
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-23 14:27:46 -0300

    feat: add button styled

---
+++ src/components/Button/button.styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ButtonStyled = styled.button `
+    color: #fff;
+    background: #7899D4;
+    border-radius: .7rem;
+    cursor: pointer;
+    padding: 0.5rem 1rem ;
+    font-size: 1.2rem;
+    border: none;
+    vertical-align: 50%;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    
+    & svg {
+        background: #7899D4;
+    }
+`
