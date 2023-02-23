11a76a1995ed1c3618eeb645e7c4bc84a08be847
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-23 13:47:35 -0300

    feat: create container info and section description styled

---
+++ src/components/AnimeInfo/anime-info.styles.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import Container from "../Container/container.component";
+
+export const ContainerInfo = styled.div`
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    p  {
+        padding: 0.5rem;
+        color: white;
+        background-color: green;
+        width: 90px;
+        border-radius: 0.7rem;
+        margin-bottom: 1rem;
+    }
+
+    h2 {
+        font-size: 40px;
+        margin: 1rem;
+    }
+`
+
+export const Description = styled.section `
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin:  0 20%;
+
+    h4 {
+        line-height: 32px;
+        word-spacing: 5px;
+        font-weight: 400;
+        font-size: 1.5rem;
+    }
+
+    p {
+        margin: 2rem;
+        
+    }
+`
