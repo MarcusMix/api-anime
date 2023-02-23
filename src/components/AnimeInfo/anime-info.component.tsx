a37ef53aa951b4020e9275e58edf5996d01287bf
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-23 13:04:13 -0300

    feat: add fetch single anime and info about

--- src/components/AnimeInfo/anime-info.component.tsx
+++ src/components/AnimeInfo/anime-info.component.tsx
@@ -1,32 +1,48 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
+
 const AnimeInfo = () => {
 
-    const [anime, setAnime] = useState<[]>([])
+    const [anime, setAnime] = useState<any>([])
+
+    const [loading, setLoading] = useState<boolean>(true)
 
     let params = useParams()
-
-    const fetchSingleAnime = async () => {
-        const response = await fetch(`https://kitsu.io/api/edge/anime/${params.id}`)
-        const dataFetch = await response.json()
-        setAnime(dataFetch.data)
-    }
     
-    console.log(anime)
     useEffect(() => {
+        const fetchSingleAnime = async () => {
+            try {
+                setLoading(true)
+                const response = await fetch(`https://kitsu.io/api/edge/anime/${params.id}`)
+                const responseJSON = await response.json()
+                setAnime(responseJSON.data.attributes)
+            } catch (error) {
+                console.log(error)
+            } finally {
+                setLoading(false)
+            }
+        }
         fetchSingleAnime()
     }, [])
     
+    console.log(anime)
+    
+
+    {loading && <div>loading...</div>}
+  
 
     return (
-        <>
-            {anime.map((value: any) => <p>{value.attributes.title}</p>
-                
-            )}
-        </>
-    
-    )
+
+            <>
+                <img src={anime.coverImage?.tiny} alt="" />
+                <div>
+                    <h1>{anime.canonicalTitle}</h1>
+                    <h3>{anime.description}</h3>
+                    <p>Status: {anime.status}</p>
+                </div>
+            </>
+    )   
 }
 
 export default AnimeInfo
