c318fc643422fc240f40d6834b659e51deeec5f6
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-25 11:37:31 -0300

    feat: add Wrapper and WrapperImage, media queries and styles

--- src/components/AnimeInfo/anime-info.styles.ts
+++ src/components/AnimeInfo/anime-info.styles.ts
@@ -1,8 +1,30 @@
 import styled from "styled-components";
-import Container from "../Container/container.component";
+
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin: 0;
+    padding: 0;
+    overflow-y: hidden;
+`
+
+export const WrapperImage = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    justify-items: center;
+    max-width: 100vh;
+    margin: auto;
+
+    & img {
+        display: flex;
+    }
+`
+
 
 export const ContainerInfo = styled.div`
-
     display: flex;
     justify-content: center;
     align-items: center;
@@ -29,17 +51,24 @@ export const Description = styled.section `
     align-items: center;
     justify-content: center;
     flex-direction: column;
-    margin:  0 20%;
+    margin:  0 10%;
 
     h4 {
-        line-height: 32px;
-        word-spacing: 5px;
+        line-height: 150%;
+        word-spacing: 2px;
         font-weight: 400;
         font-size: 1.5rem;
+        text-align: start;
     }
 
     p {
         margin: 2rem;
         
+    }
+
+    @media (max-width: 768px) {
+        h4 {
+            font-size: 1rem;
+        }
     }
 `
