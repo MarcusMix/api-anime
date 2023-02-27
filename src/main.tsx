d1da92b025790249ffbdaf943dbc5f60f9bc2bc6
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-27 13:55:32 -0300

    style: indentation code and add comment in imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
 import './index.css'
+
+//router
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 //pages
 import AnimeDetail from './pages/AnimeDetails.page'
@@ -10,10 +12,10 @@ import AnimeDetail from './pages/AnimeDetails.page'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path={'/'} element={<App />}/>
-      <Route path={'/anime/:id'} element={<AnimeDetail/>}/>
-    </Routes>
+      <Routes>
+        <Route path={'/'} element={<App />}/>
+        <Route path={'/anime/:id'} element={<AnimeDetail/>}/>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
 )
